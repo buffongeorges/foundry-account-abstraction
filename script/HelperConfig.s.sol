8c44468bfdb34c47626d4bb382141a453c90e43c
buffongeorges <georges-sxm@hotmail.fr>
2025-06-19 08:19:27 +0200

    lesson almost done

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,7 +2,8 @@
 
 pragma solidity ^0.8.24;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console2} from "forge-std/Script.sol";
+import {EntryPoint} from "lib/account-abstraction/contracts/core/EntryPoint.sol";
 
 contract HelperConfig is Script {
   error HelperConfig__InvalidChainId();
@@ -17,6 +18,8 @@ contract HelperConfig is Script {
   uint256 constant ZKSYNC_SEPOLIA_CHAIN_ID = 300;
   uint256 constant LOCAL_CHAIN_ID = 31337;
   address constant BURNER_WALLET = 0xc46C866e8D6E2CCa79c6Ab8a67F4b5b29Ad91e92;
+  address constant FOUNDRY_DEFAULT_WALLET = 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38;
+  address constant ANVIL_DEFAULT_ACCOUNT = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
 
   NetworkConfig public localNetworkConfig;
   mapping(uint256 chainId => NetworkConfig) public networkConfigs;
@@ -62,5 +65,13 @@ contract HelperConfig is Script {
 
 
     // deploy a mock entry point contract...
+    console2.log("Deploying mocks...");
+    vm.startBroadcast(ANVIL_DEFAULT_ACCOUNT);
+    EntryPoint entryPoint = new EntryPoint();
+    vm.stopBroadcast();
+    
+    localNetworkConfig = NetworkConfig({entryPoint: address(entryPoint), account: ANVIL_DEFAULT_ACCOUNT});
+
+    return localNetworkConfig;
   }
 }
--- test/ethereum/MinimalAccountTest.t.sol
+++ test/ethereum/MinimalAccountTest.t.sol
@@ -3,21 +3,30 @@
 pragma solidity ^0.8.24;
 
 import {Test} from "forge-std/Test.sol";
-import {MinimalAccount} from 'src/ethereum/MinimalAccount.sol';
-import {DeployMinimal} from 'src/ethereum/MinimalAccount.sol';
-import {HelperConfig} from 'script/HelperConfig.s.sol';
+import {MinimalAccount} from "src/ethereum/MinimalAccount.sol";
+import {DeployMinimal} from "script/DeployMinimal.s.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {SendPackedUserOp, PackedUserOperation, IEntryPoint} from "script/SendPackedUserOp.s.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
 contract MinimalAccountTest is Test {
+  using MessageHashUtils for bytes32;
+
   HelperConfig helperConfig;
   MinimalAccount minimalAccount;
   ERC20Mock usdc;
   uint256 AMOUNT = 1e18;
+  SendPackedUserOp sendPackedUserOp;
+
+  address randomUser = makeAddr('randomUser');
 
   function setUp() public {
     DeployMinimal deployMinimal = new DeployMinimal();
     (helperConfig, minimalAccount) = deployMinimal.deployMinimalAccount();
     usdc = new ERC20Mock();
+    sendPackedUserOp = new SendPackedUserOp();
   }
 
   // USDC Mint
@@ -38,6 +47,81 @@ contract MinimalAccountTest is Test {
     minimalAccount.execute(destination, value, functionData);
 
     // Assert
-    assertEq(usdc.balanceOf(adress(minimalAccount)), AMOUNT);
+    assertEq(usdc.balanceOf(address(minimalAccount)), AMOUNT);
+  }
+
+  function testNonOwnerCannotExecuteCommands() public {
+     // Arrange
+    assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+    address destination = address(usdc);
+    uint256 value = 0;
+    bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
+
+    // Act
+    vm.prank(randomUser);
+    vm.expectRevert(MinimalAccount.MinimalAccount__NotFromEntryPointOrOwner.selector);
+    minimalAccount.execute(destination, value, functionData);
+  }
+
+  function testRecoverSignedOp() public {
+    // Arrange
+    assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+    address destination = address(usdc);
+    uint256 value = 0;
+    bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
+    bytes memory executeCallData = abi.encodeWithSelector(MinimalAccount.execute.selector, destination, value, functionData); // this executeCallData is saying EntryPoint contract call our contract (execute) and then our contract will call USDC
+    PackedUserOperation memory packedUserOp = sendPackedUserOp.generateSignedUserOperation(executeCallData, helperConfig.getConfig(), address(minimalAccount));
+    bytes32 userOperationHash = IEntryPoint(helperConfig.getConfig().entryPoint).getUserOpHash(packedUserOp);
+
+    // Act
+    address actualSigner = ECDSA.recover(userOperationHash.toEthSignedMessageHash(), packedUserOp.signature);
+
+    // Assert
+    assertEq(actualSigner, minimalAccount.owner());
+  }
+
+
+  // 1. Sign user ops
+  // 2. Call validate userops
+  // 3. Assert the return is correct
+  function testValidationOfUserOps() public {
+    // Arrange
+    assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+    address destination = address(usdc);
+    uint256 value = 0;
+    bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
+    bytes memory executeCallData = abi.encodeWithSelector(MinimalAccount.execute.selector, destination, value, functionData); // this executeCallData is saying EntryPoint contract call our contract (execute) and then our contract will call USDC
+    PackedUserOperation memory packedUserOp = sendPackedUserOp.generateSignedUserOperation(executeCallData, helperConfig.getConfig(), address(minimalAccount));
+    bytes32 userOperationHash = IEntryPoint(helperConfig.getConfig().entryPoint).getUserOpHash(packedUserOp);
+    uint256 missingAccountFunds = 1e18;
+
+    // Act;
+    vm.prank(helperConfig.getConfig().entryPoint);
+    uint256 validationData = minimalAccount.validateUserOp(packedUserOp, userOperationHash, missingAccountFunds);
+
+    assertEq(validationData, 0); // 0 = SUCCESS, 1 = FAILURE
+  }
+
+  function testEntryPointCanExecuteCommands() public {
+    // Arrange
+    assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+    address destination = address(usdc);
+    uint256 value = 0;
+    bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
+    bytes memory executeCallData = abi.encodeWithSelector(MinimalAccount.execute.selector, destination, value, functionData); // this executeCallData is saying EntryPoint contract call our contract (execute) and then our contract will call USDC
+    PackedUserOperation memory packedUserOp = sendPackedUserOp.generateSignedUserOperation(executeCallData, helperConfig.getConfig(), address(minimalAccount));
+    bytes32 userOperationHash = IEntryPoint(helperConfig.getConfig().entryPoint).getUserOpHash(packedUserOp);
+
+    vm.deal(address(minimalAccount), 1e18);
+
+    PackedUserOperation[] memory ops = new PackedUserOperation[](1);
+    ops[0] = packedUserOp;
+
+    // Act
+    vm.prank(randomUser);
+    IEntryPoint(helperConfig.getConfig().entryPoint).handleOps(ops, payable(randomUser));
+
+    // Assert
+    assertEq(usdc.balanceOf(address(minimalAccount)), AMOUNT);
   }
 }
